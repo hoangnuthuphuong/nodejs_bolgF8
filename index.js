a0aa05a1e28360f768ceeb36023bdefe4cbcaa25
hoangnuthuphuong <hoangnuthuphuong@gmail.com>
2024-06-25 16:47:52 +0700

    commit2506

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
+const path = require("path");
 const express = require("express");
 const app = express();
-const port = 3000;
+//const handlebars = require("express-handlebars");
+const morgan = require("morgan");
+const port = 8080;
 
+//HTTP logger
+app.use(morgan("combined"));
+
+//config template engine
+app.set("views", path.join(__dirname, "views"));
+//app.set("views", "./views");
+app.set("view engine", "ejs");
+
+//khai bao route
 app.get("/", (req, res) => {
-  return res.send("Hello World!");
+  res.render("abc");
+});
+
+app.get("/hello", (req, res) => {
+  res.render("footer");
 });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+// thư mục chứa file public
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("assets")); // những file trong folder public và access đều có thể nhìn thấy bởi người dùng web
